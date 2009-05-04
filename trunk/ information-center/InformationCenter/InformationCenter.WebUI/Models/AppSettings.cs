39554b7dda487273e5e2035fb66631adf1d8fc8a
maninblack.msk@hotmail.com <maninblack.msk@hotmail.com@ce36add0-32a1-11de-8ef0-0f53c1b7e458>
2009-05-04 22:31:00 +0000

    WebUI Models source files UTF8 encoding

--- trunk/ information-center/InformationCenter/InformationCenter.WebUI/Models/AppSettings.cs
+++ trunk/ information-center/InformationCenter/InformationCenter.WebUI/Models/AppSettings.cs
@@ -1,4 +1,4 @@
-using System.Data.SqlClient;
+﻿using System.Data.SqlClient;
 
 namespace InformationCenter.WebUI.Models
 {
--- trunk/ information-center/InformationCenter/InformationCenter.WebUI/Models/ServiceCenterClient.cs
+++ trunk/ information-center/InformationCenter/InformationCenter.WebUI/Models/ServiceCenterClient.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using InformationCenter.Services;
 
 namespace InformationCenter.WebUI.Models
